3ef8dba2268657d7d5ea5d106b4182f788dac170
大祐永田 <dbank0208@gmail.com>
2022-01-15 11:54:33 +0900

    add: how to discard and regenerate

--- lib/mybutton.dart
+++ lib/mybutton.dart
@@ -5,11 +5,22 @@ import 'package:riverpod_example/myshowdialog.dart';
 
 class MyButton extends HookConsumerWidget {
 
-  var counter = Counter();
-  
+  Counter? counter = Counter();
+  late var c = counter ?? Counter();
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
+    /*
+      example
+      How to discard the Counter class and regenerate it,
+      in this case you can process it by using Future and making it asynchronous
+    */
+    counter = ref.watch(c.counterStateProvider) > 5 ? null : counter;
+    counter = ref.watch(counterProvider) > 6 ? c : counter;
+    if (counter == null) {
+      Future(() {
+        ref.read(c.counterStateProvider.state).state = 0;
+      });
+    }
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('Example')),
@@ -18,13 +29,13 @@ class MyButton extends HookConsumerWidget {
             mainAxisSize: MainAxisSize.min,
             children: [
               Text(ref.watch(counterProvider).toString()),
-              Text(ref.watch(counter.counterStateProvider).toString()),
+              counter == null ? Text("") : Text(ref.watch(c.counterStateProvider).toString()),
             ],
           )
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            MyDialog().showMyDialog(context, ref, counter);
+            MyDialog().showMyDialog(context,ref,c);
           },
           tooltip: 'Increment',
           child: Icon(Icons.add),
--- lib/myshowdialog.dart
+++ lib/myshowdialog.dart
@@ -5,35 +5,39 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'mycounter.dart';
 
 class MyDialog {
-  Future<void> showMyDialog(BuildContext context, WidgetRef ref, Counter counter) async {
-    showDialog<void>(
+  showMyDialog(BuildContext context, WidgetRef ref, Counter counter) {
+    showDialog(
       context: context,
       barrierDismissible: false,
       builder: (BuildContext context) {
-        return AlertDialog(
-          alignment: Alignment.topCenter,
-          content: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              FloatingActionButton(
-                onPressed: () {
-                  ref.watch(counterProvider);
-                  ref.read(counterProvider.notifier).state++;
-
-                  ref.watch(counter.counterStateProvider);
-                  ref.read(counter.counterStateProvider.state).state++;
-                },
-                tooltip: 'Increment',
-                child: Icon(Icons.add),
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              alignment: Alignment.topCenter,
+              content: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  FloatingActionButton(
+                    onPressed: () {
+                      setState(() {
+                          ref.read(counterProvider.notifier).state++;
+                          ref.read(counter.counterStateProvider.state).state++;
+                      });
+                    },
+                    tooltip: 'Increment',
+                    child: Icon(Icons.add),
+                  ),
+                  Text(ref.watch(counterProvider).toString()),
+                  TextButton(
+                    child: Text('Close'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
               ),
-              TextButton(
-                child: Text('Close'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          ),
+            );
+          },
         );
       },
     );
